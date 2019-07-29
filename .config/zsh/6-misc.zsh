6873fc2fa362f541dd3b5015f9093ca4463c0835
Daniele Lasagno <danielelasagno@gmail.com>
2019-07-29 17:43:04 +0200

    Added colors to man/less

--- .config/zsh/6-misc.zsh
+++ .config/zsh/6-misc.zsh
@@ -12,4 +12,16 @@
 
 
 # ▰▰▰ load wal ▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
 #(cat ~/.cache/wal/sequences &)
+
+
+# ▰▰▰ set man/less colors ▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
+export LESS_TERMCAP_md=$(tput bold; tput setaf 1)   # begin bold
+export LESS_TERMCAP_mb=$(tput bold; tput setaf 3)   # begin blink
+export LESS_TERMCAP_me=$(tput sgr0)                 # reset bold/blink
+export LESS_TERMCAP_so=$(tput rev; tput setaf 3)    # begin reverse video
+export LESS_TERMCAP_se=$(tput sgr0)                 # reset reverse video
+export LESS_TERMCAP_us=$(tput setaf 2)              # begin underline
+export LESS_TERMCAP_ue=$(tput sgr0)                 # reset underline
