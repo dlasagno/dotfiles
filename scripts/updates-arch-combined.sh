1dd64bfb13ac53f1851710f0ab55ec895d8fdf2c
Daniele Lasagno <danielelasagno@gmail.com>
2019-12-23 11:00:19 +0100

    Improved updates-arch-combined.sh

--- scripts/updates-arch-combined.sh
+++ scripts/updates-arch-combined.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env sh
+# Counts the number of updates available on an arch based system
+# yay should be installed for aur
+# checkupadtes should be installed for repo, but yay works too
+# nothing is printed if the aren't any updates available
+# -t --total prints the total number of updates without distinguishing aur and repo
+# -c --compact just prints numbers without text
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+if command -v checkupdates > /dev/null; then
+    updates_arch=$(checkupdates 2> /dev/null | wc -l )
+else
+    updates_arch=$(yay -Qu --repo 2> /dev/null | wc -l )
+fi
+
+if ! [ $updates_arch ]; then
     updates_arch=0
 fi
 
 if ! updates_aur=$(yay -Qu --aur 2> /dev/null | wc -l); then
-# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-# if ! updates_aur=$(pikaur -Qua 2> /dev/null | wc -l); then
     updates_aur=0
 fi
 
