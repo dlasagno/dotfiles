29c13a99139e2836fd68536ecc10d8827ee70bbc
Daniele Lasagno <danielelasagno@gmail.com>
2020-01-12 16:08:30 +0100

    Refactored polybar from the ground up

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-#       ___         ___                                                   ___           ___     
-#      /\  \       /\  \                                   _____         /\  \         /\  \    
-#     /::\  \     /::\  \                       ___       /::\  \       /::\  \       /::\  \   
-#    /:/\:\__\   /:/\:\  \                     /|  |     /:/\:\  \     /:/\:\  \     /:/\:\__\  
-#   /:/ /:/  /  /:/  \:\  \   ___     ___     |:|  |    /:/ /::\__\   /:/ /::\  \   /:/ /:/  /  
-#  /:/_/:/  /  /:/__/ \:\__\ /\  \   /\__\    |:|  |   /:/_/:/\:|__| /:/_/:/\:\__\ /:/_/:/__/___
-#  \:\/:/  /   \:\  \ /:/  / \:\  \ /:/  /  __|:|__|   \:\/:/ /:/  / \:\/:/  \/__/ \:\/:::::/  /
-#   \::/__/     \:\  /:/  /   \:\  /:/  /  /::::\  \    \::/_/:/  /   \::/__/       \::/~~/~~~~ 
-#    \:\  \      \:\/:/  /     \:\/:/  /   ~~~~\:\  \    \:\/:/  /     \:\  \        \:\~~\     
-#     \:\__\      \::/  /       \::/  /         \:\__\    \::/  /       \:\__\        \:\__\    
-#      \/__/       \/__/         \/__/           \/__/     \/__/         \/__/         \/__/    
-
+# Script to launch polybar bars
 
 # Terminate already running bar instances
 killall -q polybar
@@ -22,11 +11,23 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Load wal colors
 (cat ~/.cache/wal/sequences &)
 
-# Launch bar1 and bar2
-MONITOR=DP-1 polybar bar1 -r &
-MONITOR=HDMI-0 polybar bar2 -r &
-# for m in $(polybar --list-monitors | cut -d":" -f1); do
-#     MONITOR=$m polybar --reload example &
-# done
+# Choose the primary bar based on the type of computer
+case $(cat /sys/class/dmi/id/chassis_type) in
+  9|10|19)
+    PRIMARY_BAR=bar-laptop
+    ;;
+  *)
+    PRIMARY_BAR=bar-primary
+    ;;
+esac
+
+# Launch polybar on every monitor
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  if [ ! -z "$(polybar --list-monitors | awk -v m="$m" '/m/ && /primary/')" ]; then
+    MONITOR=$m polybar --reload $PRIMARY_BAR &
+  else
+    MONITOR=$m polybar --reload bar-secondary &
+  fi
+done
 
 echo "Bars launched..."
