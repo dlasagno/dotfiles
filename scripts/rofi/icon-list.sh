56905b34fbd7ed3e0aacf98378370cccb71f4718
Daniele Lasagno <danielelasagno@gmail.com>
2020-02-19 10:42:56 +0100

    Moved icon lists to centrilize more the configuration

--- scripts/rofi/icon-list.sh
+++ scripts/rofi/icon-list.sh
@@ -3,9 +3,14 @@
 # Give rofi a list of unicode icons from remix icon to copy.
 # Shows the selected icon in dunst if running.
 
-remixicon_modi="\"$HOME/scripts/rofi/modi/copy-unicode.sh\" \"$HOME/.config/remix-icon-list\""
-fontawesome_modi="\"$HOME/scripts/rofi/modi/copy-unicode.sh\" \"$HOME/.config/fontawesome-list\""
-emoji_modi="\"$HOME/scripts/rofi/modi/copy-unicode.sh\" \"$HOME/.config/emoji-list\""
+unicode_modi_path="$HOME/scripts/rofi/modi/copy-unicode.sh"
+remixicon_list="${XDG_DATA_HOME:-$HOME/.local/share}/miele/remixicon-list"
+fontawesome_list="${XDG_DATA_HOME:-$HOME/.local/share}/miele/fontawesome-list"
+emoji_list="${XDG_DATA_HOME:-$HOME/.local/share}/miele/emoji-list"
+
+remixicon_modi="\"$unicode_modi_path\" \"$remixicon_list\""
+fontawesome_modi="\"$unicode_modi_path\" \"$fontawesome_list\""
+emoji_modi="\"$unicode_modi_path\" \"$emoji_list\""
 
 
 # Check if xclip is installed(in order to copy)
