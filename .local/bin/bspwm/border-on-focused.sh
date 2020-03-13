b6ccf16a0f2a153e0eef8317539039f12762e6e3
Daniele Lasagno <danielelasagno@gmail.com>
2020-03-13 15:00:22 +0100

    Now the border is applied only with multiple windows on screen

--- .local/bin/bspwm/border-on-focused.sh
+++ .local/bin/bspwm/border-on-focused.sh
@@ -2,12 +2,23 @@
 
 # Puts a border only on the focused windows
 # The script requires the width of border as an argument
+# If the focused window is alone the border isn't applied
 
+# Wait for a window to get focus
 bspc subscribe node_focus | while read -r event; do
   node=$(echo "$event" | awk '{print $4}')
+  desktop=$(echo "$event" | awk '{print $3}')
+
+  # Remove border from the previous focused window
   if [ -n "$last_node" ]; then
     bspc config -n "$last_node" border_width 0
   fi
-  bspc config -n "$node" border_width "$1"
+
+  # Apply border to the focused window if not alone on its desktop
+  windows_number="$(bspc query -N -d "$desktop" -n .leaf | wc -l)"
+  if [ "$windows_number" -gt 1 ]; then
+    bspc config -n "$node" border_width "$1"
+  fi
+
   last_node="$node"
 done
